4dbdd0789946dc5cb3fed067c285ac804649bd52
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2025-11-16 22:19:59 -0300

    adiciona CORS nas properties

--- orch/src/main/java/br/edu/ifrs/orch/resource/UsersLoginResource.java
+++ orch/src/main/java/br/edu/ifrs/orch/resource/UsersLoginResource.java
@@ -5,7 +5,10 @@ import br.edu.ifrs.orch.dto.UserSignupRequestDTO;
 import br.edu.ifrs.orch.dto.UserTokenResponseDTO;
 import br.edu.ifrs.orch.service.UsersLoginService;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
