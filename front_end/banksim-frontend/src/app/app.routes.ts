941a40bff5dbc3349c3934069de11d73b501418a
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2025-11-17 19:51:22 -0300

    adiciona rota customer

--- front_end/banksim-frontend/src/app/app.routes.ts
+++ front_end/banksim-frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth';
 import { DepositComponent } from './pages/deposit/deposit';
+import { CustomerComponent } from './pages/customer/customer';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: AuthComponent },
   { path: 'deposit', component: DepositComponent },
+  { path: 'customer', component: CustomerComponent },
+  
+  // fallback (opcional mas recomendado)
+  { path: '**', redirectTo: 'login' }
 ];  
