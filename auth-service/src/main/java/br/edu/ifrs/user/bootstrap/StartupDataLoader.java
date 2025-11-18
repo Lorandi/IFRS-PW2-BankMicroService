5ac496182bd3d79072f245553bd82183dbe39c9f
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2025-11-18 18:26:41 -0300

    adiciona servico com vulnerabilidade

--- auth-service/src/main/java/br/edu/ifrs/user/bootstrap/StartupDataLoader.java
+++ auth-service/src/main/java/br/edu/ifrs/user/bootstrap/StartupDataLoader.java
@@ -1,8 +1,10 @@
 package br.edu.ifrs.user.bootstrap;
 
 import br.edu.ifrs.user.dto.SignupRequestDTO;
+import br.edu.ifrs.user.enums.RoleTypeEnum;
 import br.edu.ifrs.user.repository.UserRepository;
 import br.edu.ifrs.user.service.LoginService;
+import br.edu.ifrs.user.service.UserService;
 import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
@@ -17,6 +19,9 @@ public class StartupDataLoader {
     @Inject
     LoginService loginService;
 
+    @Inject
+    UserService userService;
+
     void init(@Observes StartupEvent ev) {
 
         // Para não duplicar se reiniciar o servidor
@@ -28,6 +33,10 @@ public class StartupDataLoader {
         loginService.signup(new SignupRequestDTO("admin", "a", "11122233344"));
         loginService.signup(new SignupRequestDTO("user1", "a", "22233344455"));
         loginService.signup(new SignupRequestDTO("auditor1", "a", "33344455566"));
+
+        userService.changeRole(1L, RoleTypeEnum.ADMIN);
+        userService.changeRole(3L, RoleTypeEnum.AUDITOR);
+
 
         System.out.println("=== Usuários iniciais criados via signup() ===");
     }
